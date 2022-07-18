57357e2ac835570e96cf82cfe09b6069996b4675
CasparLmao <96575544+CasparLmao@users.noreply.github.com>
2022-07-18 16:43:49 +0100

    Update stx_example.lua

--- Stx Console/stx_example.lua
+++ Stx Console/stx_example.lua
@@ -1,4 +1,4 @@
-local Console =  loadstring(game:HttpGet("https://raw.githubusercontent.com/CasparLmao/Ui-Libs/Main/stx/stx.lua"))()
+local Console =  loadstring(game:HttpGet("https://raw.githubusercontent.com/CasparLmao/Ui-Libs/Main/Stx%20Console/stx.lua"))()
 local ConsoleLog = Console:Window({
     Title = "[STX_Console] TEXT TITLE",
     Position = UDim2.new(0.5, 0, 0.5, 0),
